4bd538147ddf389fedaaac5ec50c06d80f8a6b1e
Eduardo Carrara <eduardo-carrara@cookpad.jp>
2020-07-01 22:15:52 +0100

    [kotlinlang-coroutines-codelab] Aggregate function implementation

--- kotlinlang-coroutines-codelab/src/tasks/Aggregation.kt
+++ kotlinlang-coroutines-codelab/src/tasks/Aggregation.kt
@@ -14,5 +14,12 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> = this
+        .groupingBy { it.login }
+        .reduce { _, totalUser, user ->
+            totalUser.copy(
+                    contributions = totalUser.contributions + user.contributions
+            )
+        }
+        .values.toList()
+        .sortedByDescending { it.contributions }
