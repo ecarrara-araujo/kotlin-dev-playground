c1f76fbfa1f74ca229b0fc80b3f8ced8e97b1a5e
Eduardo Carrara <eduardo-carrara@cookpad.jp>
2020-07-04 14:12:52 +0100

    [kotlinlang-coroutines-codelab] Coroutines Concurrency

--- kotlinlang-coroutines-codelab/src/tasks/Request5Concurrent.kt
+++ kotlinlang-coroutines-codelab/src/tasks/Request5Concurrent.kt
@@ -3,6 +3,26 @@ package tasks
 import contributors.*
 import kotlinx.coroutines.*
 
-suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> {
+    return coroutineScope {
+        val repos = service
+                .getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .body() ?: listOf()
+
+        val deferreds = repos.map { repo ->
+            async {
+                log("starting loading for ${repo.name}")
+                service
+                        .getRepoContributors(req.org, repo.name)
+                        .also { logUsers(repo, it) }
+                        .bodyList()
+            }
+        }
+
+        deferreds
+                .awaitAll()
+                .flatten()
+                .aggregate()
+    }
 }
