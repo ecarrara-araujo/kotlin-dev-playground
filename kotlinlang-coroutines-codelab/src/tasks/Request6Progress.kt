1d9de0a098f817aa5c6d01b0942677b4b8ec3de5
Eduardo Carrara <eduardo-carrara@cookpad.jp>
2020-07-04 14:36:43 +0100

    [kotlinlang-coroutines-codelab] Updating loading progress

--- kotlinlang-coroutines-codelab/src/tasks/Request6Progress.kt
+++ kotlinlang-coroutines-codelab/src/tasks/Request6Progress.kt
@@ -1,11 +1,28 @@
 package tasks
 
-import contributors.*
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.logRepos
+import contributors.logUsers
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsProgress(
-    service: GitHubService,
-    req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
+        service: GitHubService,
+        req: RequestData,
+        updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    coroutineScope {
+        val repos = service.getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .bodyList()
+        var allUsers = emptyList<User>()
+        for ((index, repo) in repos.withIndex()) {
+            val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, index == repos.lastIndex)
+        }
+    }
 }
