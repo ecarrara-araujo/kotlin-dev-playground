53ee7afa9cf761bd2bc02069efa700f6d242b3e4
Eduardo Carrara <eduardo-carrara@cookpad.jp>
2020-07-04 14:20:32 +0100

    [kotlinlang-coroutines-codelab] Coroutines Global Scope and Cancelation

--- kotlinlang-coroutines-codelab/src/tasks/Request5NotCancellable.kt
+++ kotlinlang-coroutines-codelab/src/tasks/Request5NotCancellable.kt
@@ -1,9 +1,36 @@
 package tasks
 
-import contributors.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.log
+import contributors.logRepos
+import contributors.logUsers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
 
 suspend fun loadContributorsNotCancellable(service: GitHubService, req: RequestData): List<User> {
-    TODO()
+    val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+    val deferreds = repos.map { repo ->
+        GlobalScope.async {
+            log("starting loading for ${repo.name}")
+            delay(3000)
+            service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+        }
+    }
+
+    return deferreds
+            .awaitAll()
+            .flatten()
+            .aggregate()
+
 }
