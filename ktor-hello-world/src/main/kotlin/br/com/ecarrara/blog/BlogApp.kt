33369905c98f4c33a56a0012c26c9555b8034a99
Eduardo Carrara <eduardo-carrara@cookpad.jp>
2020-07-18 18:16:17 +0100

    [kto-hello] Create a application module for setup

--- ktor-hello-world/src/main/kotlin/br/com/ecarrara/blog/BlogApp.kt
+++ ktor-hello-world/src/main/kotlin/br/com/ecarrara/blog/BlogApp.kt
@@ -1,19 +1,32 @@
 package br.com.ecarrara.blog
 
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
-fun main() {
-    embeddedServer(Netty, 8080) {
-        routing {
-            get("/") {
-                call.respondText("My Example Blog", ContentType.Text.Html)
-            }
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get {
+            call.respondText("My Example Blog2", ContentType.Text.Html)
         }
-    }.start(wait = true)
+    }
+}
+
+fun main() {
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        watchPaths = listOf("BlogAppKt"),
+        module = Application::module
+    ).start()
 }
